6719d8943c222f1e4c75edf8fb82c2b1d415e6bc
Some1isHeree <86526585+Some1isHeree@users.noreply.github.com>
2021-10-01 20:08:20 +0200

    Add files via upload

--- Files and data/LeClick.bat
+++ Files and data/LeClick.bat
@@ -1,6 +1,6 @@
 
 @echo off
-title LeClick v1.1
+title LeClick v1.3 - Upgrade time!
 color 0f
 
 
@@ -11,6 +11,7 @@ cls
 for /f "tokens=*" %%a in ('type money.txt ') do set money=%%a
 for /f "tokens=*" %%a in ('type login.txt ') do set login=%%a
 for /f "tokens=*" %%a in ('type passw.txt ') do set passw=%%a
+for /f "tokens=*" %%a in ('type multi.txt ') do set multi=%%a
 
 goto login
 
@@ -52,7 +53,7 @@ echo.
 echo ------------------------
 echo -- Hello %login%!
 echo -- Welcome to LeClick!
-echo -- Version 1.1
+echo -- Version 1.3
 echo ------------------------
 echo -- Money: %money%
 echo ------------------------
@@ -120,29 +121,31 @@ cls
 
 echo.
 echo.
-echo -------------------
-echo C C C C C C C C C C
-echo L L L L L L L L L L
-echo I I I I I I I I I I
-echo C C C C C C C C C C 
-echo K K K K K K K K K K
-echo -------------------
+echo -----------------------
+echo C C C C C C C C C C C C
+echo L L L L L L L L L L L L
+echo I I I I I I I I I I I I
+echo C C C C C C C C C C C C
+echo K K K K K K K K K K K K
+echo -----------------------
 echo Money: %money%
 echo Name: %login%
-echo -------------------
+echo -----------------------
 echo press 1 to click
 echo press 2 to save
-echo press 3 to exit
-echo -------------------
-CHOICE /C:123 /N >nul
-REM CHOICE /C 123 /N >nul
+echo press 3 to upgrade
+echo press 4 to exit
+echo -----------------------
+CHOICE /C:1234 /N >nul
+REM CHOICE /C 1234 /N >nul
 if %errorlevel%==1 goto click
 if %errorlevel%==2 goto save
-if %errorlevel%==3 goto menu
+if %errorlevel%==3 goto upgrade
+if %errorlevel%==4 goto menu
 goto game
 
 :click
-set/a money=%money%+1
+set/a money=money+multi
 goto game
 
 :save
@@ -151,9 +154,81 @@ echo.
 echo.
 echo -- Saving...
 echo %money% > money.txt
+echo %multi% > multi.txt
 echo -- Saved Successfully!
 pause
 goto game
+
+::upgrades
+
+:upgrade
+cls
+echo.
+echo.
+echo -----------------------
+echo "c" stands for 
+echo "money per click"
+echo press 1 to buy +1 c
+echo (100$)
+echo press 2 to buy +5 c
+echo (1 000$)
+echo press 3 to buy +10 c
+echo (10 000$)
+echo press 4 to buy +15 c
+echo (100 000$) 
+echo press 5 to buy +20 c
+echo (1 000 000$)
+echo.
+echo press 6 to go back
+echo -----------------------
+CHOICE /C:123456 /N >nul
+REM CHOICE /C 123450 /N >nul
+if %errorlevel%==1 goto 1
+if %errorlevel%==2 goto 2
+if %errorlevel%==3 goto 3
+if %errorlevel%==4 goto 4
+if %errorlevel%==5 goto 5
+if %errorlevel%==6 goto game 
+
+:1
+set/a multi=multi+1
+set/a money=money-100
+echo.
+echo.
+echo Done! c = %multi%
+goto upgrade
+
+:2
+set/a multi=multi+5
+set/a money=money-1000
+echo.
+echo.
+echo Done! c = %multi%
+goto upgrade
+
+:3
+set/a multi=multi+10
+set/a money=money-10000
+echo.
+echo.
+echo Done! c = %multi%
+goto upgrade
+
+:4
+set/a multi=multi+15
+set/a money=money-100000
+echo.
+echo.
+echo Done! c = %multi%
+goto upgrade
+
+:5
+set/a multi=multi+20
+set/a money=money-1000000
+echo.
+echo.
+echo Done! c = %multi%
+goto upgrade
 
 
 
---
+++ Launcher.bat
@@ -0,0 +1,17 @@
+Title LeClick Opener 30303030
+
+@echo off
+cls
+
+echo Launching LeClick v1.3
+
+timeout 4 >nul
+
+cd %cd%
+cd *\Files and Data\
+start LeClick.bat
+
+echo Closing Launcher...
+timeout 4 >nul
+
+exit
